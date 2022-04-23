ae139616ad52cd3f19689430b62e6b2e04fe8929
enes <enesarda22@gmail.com>
2022-04-23 14:30:17 +0300

    add DataBase class

--- database.py
+++ database.py
@@ -1,3 +1,10 @@
+import os
+import glob
+import pickle
+
+import random
+
+
 class Sample:
 
     def __init__(self, speech, noise, name):
@@ -7,3 +14,31 @@ class Sample:
 
     def get_noisy(self):
         return self.speech + self.noise
+
+
+class DataBase:
+
+    def __init__(self, n_samples=None):
+
+        self.samples = []
+
+        if not os.path.isdir("data"):
+            print("data/ path doesn't exist!")
+            return
+
+        pickle_files = glob.glob("data/*.pkl")
+
+        if n_samples is None:
+            for pickle_file in pickle_files:
+                with open(pickle_file, "rb") as file:
+                    sample_obj = pickle.load(file)
+                    self.samples.append(sample_obj)
+
+        else:
+            random.shuffle(pickle_files)
+            for pickle_file in pickle_files[:n_samples]:
+                with open(pickle_file, "rb") as file:
+                    sample_obj = pickle.load(file)
+                    self.samples.append(sample_obj)
+
+        print(f"{len(self.samples)} pickle files are loaded.")
